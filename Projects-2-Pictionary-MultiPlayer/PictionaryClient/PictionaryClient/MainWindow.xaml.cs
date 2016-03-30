e3a8d95c3c3123296a67288282f7a35894fa2671
John Bergen Friesen <johnfriesen140@hotmail.com>
2016-03-30 19:33:03 -0400

    service stuff is there but not working yet

--- Projects-2-Pictionary-MultiPlayer/PictionaryClient/PictionaryClient/MainWindow.xaml.cs
+++ Projects-2-Pictionary-MultiPlayer/PictionaryClient/PictionaryClient/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,19 +15,28 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Threading;
 
+using System.ServiceModel;
+using System.Web.Script.Serialization;
+using PictionaryClient.PictionaryServiceRef;
+
 
 namespace PictionaryClient
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    [CallbackBehavior(ConcurrencyMode = ConcurrencyMode.Reentrant, UseSynchronizationContext = false)]
+    public partial class MainWindow : Window, IUserCallback
     {
-        Point drawPoint = new Point();
+        Point _drawPoint = new Point();
+        private UserClient _cnvsBrd = null;
+        private string _userName;
+
         public MainWindow()
         {
             InitializeComponent();
             this.Loaded += MainWindow_Loaded;
+            connectToPictionaryGame();
 
         }
 
@@ -45,14 +55,47 @@ namespace PictionaryClient
             DrawWord dw = dwList[r.Next(0, dwList.Count)]; //pick a random DrawWord from the list
             MyHintProperty = dw.wordHintFirstLetter_;  //set the hintProperty
 
-
+            
             this.DataContext = this;
+        }
+
+        private void connectToPictionaryGame()
+        {
+            try
+            {
+                // Configure the ABCs of using the MessageBoard service
+                //DuplexChannelFactory<IUser> channel = new DuplexChannelFactory<IUser>(this, "User");
+
+                // Activate a MessageBoard object
+                //msgBrd = channel.CreateChannel();
+                _cnvsBrd = new UserClient(new InstanceContext(this));
+                // TODO: get better user asking code for getting username and remove this crowd thing
+                Random ran = new Random();
+                _userName = "Player:" + ran.Next(0, 1000);
+                if (_cnvsBrd.Join(_userName))
+                {
+                    
+                    // TODO: currently we have it so that new players cant join mid game
+                    whiteBoard.Children.Clear();
+
+                }
+                else
+                {
+                    // Alias rejected by the service so nullify service proxies
+                    _cnvsBrd = null;
+                    MessageBox.Show("ERROR: Alias in use. Please try again.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
 
         private void whiteBoard_mouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
             if (e.ButtonState == MouseButtonState.Pressed)
-                drawPoint = e.GetPosition(whiteBoard);
+                _drawPoint = e.GetPosition(whiteBoard);
         }
 
         private void Canvas_MouseMove_1(object sender, System.Windows.Input.MouseEventArgs e)
@@ -64,12 +107,12 @@ namespace PictionaryClient
                 line.Stroke = MyColorProperty;
                 line.StrokeThickness = MyMarkerThicknessProperty;
 
-                line.X1 = drawPoint.X;
-                line.Y1 = drawPoint.Y;
+                line.X1 = _drawPoint.X;
+                line.Y1 = _drawPoint.Y;
                 line.X2 = e.GetPosition(whiteBoard).X;
                 line.Y2 = e.GetPosition(whiteBoard).Y;
 
-                drawPoint = e.GetPosition(whiteBoard);
+                _drawPoint = e.GetPosition(whiteBoard);
 
                 whiteBoard.Children.Add(line);
 
@@ -113,8 +156,6 @@ namespace PictionaryClient
         // Using a DependencyProperty as the backing store for MyMarkerThicknessProperty.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty MyMarkerThicknessPropertyProperty =
             DependencyProperty.Register("MyMarkerThicknessProperty", typeof(double), typeof(MainWindow), new PropertyMetadata(5.0));
-
-
 
 
 
@@ -166,6 +207,33 @@ namespace PictionaryClient
         private void purpleMarker_MouseDown(object sender, MouseButtonEventArgs e)
         {
             MyColorProperty = Brushes.Purple;
+        }
+
+        public delegate void CanvasUpdateDelegate(string jsonLine);
+
+        public void SendLine(string jsonLine)
+        {
+            if (this.Dispatcher.Thread == System.Threading.Thread.CurrentThread)
+            {
+                try
+                {
+                    Line line = new JavaScriptSerializer().Deserialize<Line>(jsonLine);
+
+                    whiteBoard.Children.Add(line);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else
+                this.Dispatcher.BeginInvoke(new CanvasUpdateDelegate(SendLine), new object[] { jsonLine });
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            _cnvsBrd?.Leave(_userName);
+            base.OnClosing(e);
         }
     }
 }
--- Projects-2-Pictionary-MultiPlayer/PictionaryClient/PictionaryLibrary/PictionaryLibrary.cs
+++ Projects-2-Pictionary-MultiPlayer/PictionaryClient/PictionaryLibrary/PictionaryLibrary.cs
@@ -76,6 +76,7 @@ namespace PictionaryLibrary
             updateAllUsers();
         }
 
+        // TODO: make this return all line later
         public string GetLine()
         {
             return drawLine;
