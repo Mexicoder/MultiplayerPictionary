26e99191bcee66779ab7b606dbb8cefb89147a46
john friesen <superjohn140@gmail.com>
2016-03-30 09:42:56 -0400

    dictionary client with some non working service code

---
+++ Projects-2-Pictionary-MultiPlayer/PictionaryClient/PictionaryLibrary/PictionaryLibrary.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Windows;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace PictionaryLibrary
+{
+    public interface IUserCallback
+    {
+        [OperationContract(IsOneWay = true)]
+        void SendCanvas(Canvas canvas);
+    }
+
+    /*----------------------------------- Service Contracts ----------------------------------*/
+
+    [ServiceContract(CallbackContract = typeof(IUserCallback))]
+    public interface IUser
+    {
+        [OperationContract]
+        bool Join(string name);
+        [OperationContract(IsOneWay = true)]
+        void Leave(string name);
+        [OperationContract(IsOneWay = true)]
+        void PostCanvas(Canvas canvas);
+        [OperationContract]
+        Canvas GetCanvas();
+    }
+
+    /*--------------------------------- Service Implementation -------------------------------*/
+
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
+    public class DrawCanvasBoard : IUser
+    {
+        private Dictionary<string, IUserCallback> userCallbacks = new Dictionary<string, IUserCallback>();
+        private Canvas drawCanvas;
+
+        /*----------------------------------- IUser methods ----------------------------------*/
+
+        public bool Join(string name)
+        {
+            if (userCallbacks.ContainsKey(name.ToUpper()))
+                // User alias must be unique
+                return false;
+            else
+            {
+
+                // Retrieve client's callback proxy
+                IUserCallback cb = OperationContext.Current.GetCallbackChannel<IUserCallback>();
+
+                // Save alias and callback proxy    
+                userCallbacks.Add(name.ToUpper(), cb);
+                drawCanvas = new Canvas();
+
+                return true;
+            }
+        }
+
+        public void Leave(string name)
+        {
+            if (userCallbacks.ContainsKey(name.ToUpper()))
+            {
+                userCallbacks.Remove(name.ToUpper());
+                drawCanvas = new Canvas();
+            }
+        }
+
+        public void PostCanvas(Canvas canvas)
+        {
+            drawCanvas = canvas;
+            updateAllUsers();
+        }
+
+        public Canvas GetCanvas()
+        {
+            return drawCanvas;
+        }
+
+        /*---------------------------------- Helper methods ----------------------------------*/
+
+        private void updateAllUsers()
+        {
+            //TODO try getting rid of this 
+            Canvas c = drawCanvas;
+            foreach (IUserCallback cb in userCallbacks.Values)
+                cb.SendCanvas(c);
+        }
+
+
+    }
+}
---
+++ Projects-2-Pictionary-MultiPlayer/PictionaryClient/PictionaryService/Program.cs
@@ -0,0 +1,37 @@
+﻿using PictionaryLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PictionaryService
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost servHost = null;
+            try
+            {
+                // Create the service host 
+                servHost = new ServiceHost(typeof(DrawCanvasBoard));
+
+                // Start the service
+                servHost.Open();
+                Console.WriteLine("Service started. Press a key to quit.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadKey();
+                if (servHost != null)
+                    servHost.Close();
+            }
+        }
+    }
+}
